537b70041b8c6521766e2bf85d6f12ca0bd475a5
DanieII <danielbogdanov1748@gmail.com>
2024-12-21 15:49:58 +0200

    Add Bulgaria and Sofia queries for fetching OSM data

--- backend/saveTrails.js
+++ backend/saveTrails.js
@@ -1,11 +1,10 @@
 import osmtogeojson from "osmtogeojson";
 import { length } from "@turf/length";
-import Trail from "./models/Trail.js";
 import mongoose from "mongoose";
+import Trail from "./models/trailModel.js";
+import connectToDB from "./config/db.js";
 
-const getOSMTrailsData = async () => {
-  // Getting trails in Sofia
-  const query = `[out:json][timeout:25];
+const querySofia = `[out:json][timeout:25];
                 // fetch area “Sofia” to search in
                 area(id:3604283101)->.searchArea;
                 // gather results
@@ -13,90 +12,99 @@ const getOSMTrailsData = async () => {
                 // print results
                 out geom;`;
 
-  const response = await fetch("https://overpass-api.de/api/interpreter", {
-    method: "POST",
-    body: "data=" + encodeURIComponent(query),
-  });
-  const osmData = await response.json();
-  const osmDataAsGeoJson = osmtogeojson(osmData);
+const queryBulgaria = `[out:json][timeout:25];
+                // fetch area “Bulgaria” to search in
+                // area(id:3600186382)->.searchArea;
+                // gather results
+                nwr["route"="hiking"]["network"!="iwn"]["network"!="nwn"](area.searchArea);
+                // print results
+                out geom;`;
 
-  return osmDataAsGeoJson;
+const getOSMTrailsData = async () => {
+    const response = await fetch("https://overpass-api.de/api/interpreter", {
+        method: "POST",
+        body: "data=" + encodeURIComponent(querySofia),
+    });
+    const osmData = await response.json();
+    const osmDataAsGeoJson = osmtogeojson(osmData);
+
+    return osmDataAsGeoJson;
 };
 
 const getTrailStartingCoordinates = (geometry) => {
-  if (geometry.type === "MultiLineString") {
-    return geometry.coordinates[0][0];
-  }
+    if (geometry.type === "MultiLineString") {
+        return geometry.coordinates[0][0];
+    }
 
-  return geometry.coordinates[0];
+    return geometry.coordinates[0];
 };
 
 const getTrailLocation = async (geometry) => {
-  const [lon, lat] = getTrailStartingCoordinates(geometry);
-  const response = await fetch(
-    `https://nominatim.openstreetmap.org/reverse.php?format=json&lat=${lat}&lon=${lon}&zoom=5`,
-  );
-  const data = await response.json();
+    const [lon, lat] = getTrailStartingCoordinates(geometry);
+    const response = await fetch(
+        `https://nominatim.openstreetmap.org/reverse.php?format=json&lat=${lat}&lon=${lon}&zoom=5`,
+    );
+    const data = await response.json();
 
-  return data.name;
+    return data.name;
 };
 
 const getTrailName = (properties) => {
-  if (properties.name) return properties.name;
+    if (properties.name) return properties.name;
 
-  return `${properties.name.from} - ${properties.name.to}`;
+    return `${properties.name.from} - ${properties.name.to}`;
 };
 
 const validateTrailData = (data) => {
-  const allowedGeometryTypes = Trail.schema.path("geometry.type").enumValues;
+    const allowedGeometryTypes = Trail.schema.path("geometry.type").enumValues;
 
-  if (!data.properties.name && !(data.properties.from && data.properties.to))
-    return false;
-  if (!allowedGeometryTypes.includes(data.geometry.type)) return false;
+    if (!data.properties.name && !(data.properties.from && data.properties.to))
+        return false;
+    if (!allowedGeometryTypes.includes(data.geometry.type)) return false;
 
-  return true;
+    return true;
 };
 
 const saveTrailsData = async () => {
-  console.log("Connecting to database...");
-  await mongoose.connect("mongodb://localhost:27017/bg-trail", {});
+    console.log("Connecting to database...");
+    await await connectToDB();
 
-  console.log("Fetching data from OpenStreetMap...");
-  const data = await getOSMTrailsData();
+    console.log("Fetching data from OpenStreetMap...");
+    const data = await getOSMTrailsData();
 
-  console.log("Processing trails data...");
-  const trailsToSave = [];
-  // No Promise.all() because of nominatim api request limit
-  for (const trailData of data.features) {
-    if (!validateTrailData(trailData)) continue;
+    console.log("Processing trails data...");
+    const trailsToSave = [];
+    // No Promise.all() because of nominatim api request limit
+    for (const trailData of data.features) {
+        if (!validateTrailData(trailData)) continue;
 
-    try {
-      const trailName = getTrailName(trailData.properties);
-      const trailLocation = await getTrailLocation(trailData.geometry);
-      const trailLength = Math.ceil(
-        length(trailData.geometry, { units: "kilometers" }),
-      );
+        try {
+            const trailName = getTrailName(trailData.properties);
+            const trailLocation = await getTrailLocation(trailData.geometry);
+            const trailLength = Math.ceil(
+                length(trailData.geometry, { units: "kilometers" }),
+            );
 
-      trailsToSave.push({
-        name: trailName,
-        location: trailLocation,
-        length: trailLength,
-        geometry: trailData.geometry,
-      });
-    } catch (error) {
-      console.error("Failed to process trail data:", error);
+            trailsToSave.push({
+                name: trailName,
+                location: trailLocation,
+                length: trailLength,
+                geometry: trailData.geometry,
+            });
+        } catch (error) {
+            console.error("Failed to process trail data:", error);
+        }
     }
-  }
 
-  console.log("Saving trails...");
-  try {
-    await Trail.insertMany(trailsToSave);
-    console.log("Trails saved successfully");
-  } catch (error) {
-    console.error("Failed to save trails:", error);
-  }
+    console.log("Saving trails...");
+    try {
+        await Trail.insertMany(trailsToSave);
+        console.log("Trails saved successfully");
+    } catch (error) {
+        console.error("Failed to save trails:", error);
+    }
 
-  mongoose.disconnect();
+    mongoose.disconnect();
 };
 
 saveTrailsData();
